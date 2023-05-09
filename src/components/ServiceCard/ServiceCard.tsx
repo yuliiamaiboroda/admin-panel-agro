5a3195d7b0c126e5865f4d75f3bbddd178fb6677
Iaroslav Seniuk <savuk7@icloud.com>
2023-05-09 12:56:36 +0300

    added ServiceCard component

---
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,17 @@
+export default function ServiceCard() {
+  return (
+    <div>
+      <span>
+        IMAGE-IMAGE-IMAGE
+        <br />
+        IMAGE-IMAGE-IMAGE
+        <br />
+      </span>
+      <b>This is ServiceCard!</b>
+      <p>Description</p>
+      <p>Price</p>
+      <p>Phone</p>
+      <p>Email</p>
+    </div>
+  );
+}
