a133b4cc0aee9e2feb39e47c6bc914635c774a63
Iaroslav Seniuk <savuk7@icloud.com>
2023-05-09 12:56:06 +0300

    added Services Page

---
+++ src/pages/ServicesPage/ServicesPage.tsx
@@ -0,0 +1,11 @@
+import PageTitle from 'components/PageTitle';
+import ServiceCard from 'components/ServiceCard';
+
+export default function ServicesPage() {
+  return (
+    <>
+      <PageTitle title="Company services" />
+      <ServiceCard />
+    </>
+  );
+}
