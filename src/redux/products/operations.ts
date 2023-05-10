88c0cb5ea94e092fd63e70871f0df9a7dd6fa98c
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-05-10 16:54:35 +0300

    Create create product operation

--- src/redux/products/operations.ts
+++ src/redux/products/operations.ts
@@ -15,3 +15,19 @@ export const getAllProducts = createAsyncThunk<
     thunkApi.rejectWithValue(error.message);
   }
 });
+
+export const createProduct = createAsyncThunk<
+  IProduct,
+  { title: string; description: string; image: FileList },
+  { rejectValue: string }
+>('products/createProduct', async (product, thunkApi) => {
+  try {
+    const { data } = await axios.post('/api/products/all', product, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+    return data;
+  } catch (err) {
+    const error = err as AxiosError;
+    thunkApi.rejectWithValue(error.message);
+  }
+});
