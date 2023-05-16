2e7697914fda6577a2993a574af5f3e9020e33f6
Iaroslav Seniuk <savuk7@icloud.com>
2023-05-16 17:20:18 +0300

    added features to redux

--- src/redux/services/slice.ts
+++ src/redux/services/slice.ts
@@ -1,5 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { createService, deleteService, getAllServices } from './operations';
+import {
+  createService,
+  deleteService,
+  getAllServices,
+  updateService,
+} from './operations';
 
 export interface IService {
   _id: string;
@@ -51,7 +56,7 @@ const getAllServicesFulfilledReducer = (
   };
 };
 
-const createServicesFulfilledReducer = (
+const createServiceFulfilledReducer = (
   state: IState,
   action: PayloadAction<IService, string>
 ) => {
@@ -62,7 +67,7 @@ const createServicesFulfilledReducer = (
   };
 };
 
-const deleteServicesFulfilledReducer = (
+const deleteServiceFulfilledReducer = (
   state: IState,
   action: PayloadAction<string, string>
 ) => {
@@ -70,6 +75,30 @@ const deleteServicesFulfilledReducer = (
     ...state,
     isLoading: false,
     entities: state.entities.filter(item => item._id !== action.payload),
+  };
+};
+
+const updateServiceFulfilledReducer = (
+  state: IState,
+  action: PayloadAction<IService, string>
+) => {
+  return {
+    ...state,
+    isLoading: false,
+    entities: state.entities.map(item => {
+      if (item._id === action.payload._id) {
+        return {
+          ...item,
+          title: action.payload.title,
+          description: action.payload.description,
+          imageURL: action.payload.imageURL,
+          price: action.payload.price,
+          contactMail: action.payload.contactMail,
+          contactPhone: action.payload.contactPhone,
+        };
+      }
+      return item;
+    }),
   };
 };
 
@@ -83,11 +112,14 @@ const servicesSlice = createSlice({
       .addCase(getAllServices.fulfilled, getAllServicesFulfilledReducer)
       .addCase(getAllServices.rejected, servicesRejectedReducer)
       .addCase(createService.pending, servicesPendingReducer)
-      .addCase(createService.fulfilled, createServicesFulfilledReducer)
+      .addCase(createService.fulfilled, createServiceFulfilledReducer)
       .addCase(createService.rejected, servicesRejectedReducer)
       .addCase(deleteService.pending, servicesPendingReducer)
-      .addCase(deleteService.fulfilled, deleteServicesFulfilledReducer)
-      .addCase(deleteService.rejected, servicesRejectedReducer),
+      .addCase(deleteService.fulfilled, deleteServiceFulfilledReducer)
+      .addCase(deleteService.rejected, servicesRejectedReducer)
+      .addCase(updateService.pending, servicesPendingReducer)
+      .addCase(updateService.fulfilled, updateServiceFulfilledReducer)
+      .addCase(updateService.rejected, servicesRejectedReducer),
 });
 
 export const servicesReducer = servicesSlice.reducer;
