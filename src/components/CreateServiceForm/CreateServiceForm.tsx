f1d241cdb5b87361183a1494e190ceeda2d7a5c4
Iaroslav Seniuk <savuk7@icloud.com>
2023-05-19 01:30:51 +0300

    added createService validation

--- src/components/CreateServiceForm/CreateServiceForm.tsx
+++ src/components/CreateServiceForm/CreateServiceForm.tsx
@@ -3,6 +3,7 @@ import { Formik, Form, Field } from 'formik';
 import { useAppDispatch } from 'hooks';
 import { createService } from 'redux/services';
 import UploadFileField from 'components/UploadFileField';
+import createServiceSchema from 'helpers/schemas/services/createService.schema';
 
 interface IProps {
   onSubmit?: Function;
@@ -22,6 +23,8 @@ export default function CreateServiceForm({ onSubmit }: IProps) {
         contactMail: '',
         contactPhone: '',
       }}
+      validateOnBlur
+      validationSchema={createServiceSchema}
       onSubmit={(values, actions) => {
         if (fileField.current?.files) {
           const { title, description, price, contactMail, contactPhone } =
@@ -44,31 +47,44 @@ export default function CreateServiceForm({ onSubmit }: IProps) {
         }
       }}
     >
-      <Form>
-        <label>
-          Title: <Field id="title" name="title" type="text" />
-        </label>
-        <br />
-        <label>
-          Description: <Field id="description" name="description" type="text" />
-        </label>
-        <br />
-        <UploadFileField name="image" fileRef={fileField} />
-        <br />
-        <label>
-          Price: <Field id="price" name="price" type="text" />
-        </label>
-        <br />
-        <label>
-          Contact mail: <Field id="contactMail" name="contactMail" type="text" />
-        </label>
-        <br />
-        <label>
-          Contact phone: <Field id="contactPhone" name="contactPhone" type="text" />
-        </label>
-        <br />
-        <button type="submit">Add service</button>
-      </Form>
+      {({ errors, touched }) => (
+        <Form>
+          <label>
+            Заголовок: <Field id="title" name="title" type="text" />
+            {errors.title && touched.title ? <span>{errors.title}</span> : null}
+          </label>
+          <br />
+          <label>
+            Опис: <Field id="description" name="description" type="text" />
+            {errors.description && touched.description ? (
+              <span>{errors.description}</span>
+            ) : null}
+          </label>
+          <br />
+          <UploadFileField name="image" fileRef={fileField} />
+          <br />
+          <label>
+            Ціна: <Field id="price" name="price" type="text" />
+            {errors.price && touched.price ? <span>{errors.price}</span> : null}
+          </label>
+          <br />
+          <label>
+            Контактна пошта: <Field id="contactMail" name="contactMail" type="text" />
+            {errors.contactMail && touched.contactMail ? (
+              <span>{errors.contactMail}</span>
+            ) : null}
+          </label>
+          <br />
+          <label>
+            Контактний номер: <Field id="contactPhone" name="contactPhone" type="text" />
+            {errors.contactPhone && touched.contactPhone ? (
+              <span>{errors.contactPhone}</span>
+            ) : null}
+          </label>
+          <br />
+          <button type="submit">Add service</button>
+        </Form>
+      )}
     </Formik>
   );
 }
