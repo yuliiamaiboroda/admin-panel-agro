f1d962f3fed1cce80bea7043b46999460dafc4e1
Iaroslav Seniuk <savuk7@icloud.com>
2023-05-18 15:31:35 +0300

    added loader

--- src/components/ServicesGallery/ServicesGallery.tsx
+++ src/components/ServicesGallery/ServicesGallery.tsx
@@ -1,16 +1,22 @@
 import { useAppSelector } from 'hooks';
-import { selectServices } from 'redux/services';
+import { selectIsLoading, selectServices } from 'redux/services';
 
 import ServiceCard from 'components/ServiceCard';
+import Loader from 'components/Loader';
 
 export default function ServicesGallery() {
   const services = useAppSelector(selectServices);
+  const isLoading = useAppSelector(selectIsLoading);
 
   return (
     <ul>
-      {services.map(({ _id, ...rest }) => (
-        <ServiceCard key={_id} _id={_id} {...rest} />
-      ))}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        services.map(({ _id, ...rest }) => (
+          <ServiceCard key={_id} _id={_id} {...rest} />
+        ))
+      )}
     </ul>
   );
 }
--- src/redux/services/selectors.ts
+++ src/redux/services/selectors.ts
@@ -1,3 +1,4 @@
 import type { RootState } from 'redux/store';
 
 export const selectServices = (state: RootState) => state.services.entities
+export const selectIsLoading = (state: RootState) => state.services.isLoading
