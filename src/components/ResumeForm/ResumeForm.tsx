230c66fc16bbb2da1e090f38961e30129f1052e9
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-05-21 18:03:11 +0300

    Add base form of resume creator

--- src/components/ResumeForm/ResumeForm.tsx
+++ src/components/ResumeForm/ResumeForm.tsx
@@ -1,3 +1,59 @@
+import { useRef } from 'react';
+import { Formik, Form, Field } from 'formik';
+import UploadFileField from 'components/UploadFileField';
+
 export default function ResumeForm() {
-	return <h1>This is ResumeForm!</h1>;
+  const fileField = useRef<HTMLInputElement>(null);
+
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        phone: '',
+        email: '',
+        position: '',
+        resumeFile: '',
+        comment: '',
+        agreement: false,
+      }}
+      onSubmit={(values, action) => {
+        console.log({ values, file: fileField.current?.files });
+        action.resetForm();
+      }}
+    >
+      {({ values }) => (
+        <Form>
+          <label>
+            Name: <Field id="name" name="name" type="text" />
+          </label>
+          <br />
+          <label>
+            Phone: <Field id="phone" name="phone" type="text" />
+          </label>
+          <br />
+          <label>
+            Email: <Field id="email" name="email" type="email" />
+          </label>
+          <br />
+          <label>
+            position: <Field id="position" name="position" type="text" />
+          </label>
+          <br />
+          <UploadFileField name="resumeFile" fileRef={fileField} />
+          <br />
+          <label>
+            Comment: <Field id="comment" name="comment" type="text" />
+          </label>
+          <br />
+          <label>
+            Agreement: <Field id="agreement" name="agreement" type="checkbox" />
+          </label>
+          <br />
+          <button type="submit" disabled={!values.agreement}>
+            Add resume
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
 }
