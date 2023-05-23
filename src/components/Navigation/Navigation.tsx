d7d4f97e2360900c3ec92aa62841edd5512d5473
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-05-23 20:13:22 +0300

    Create restricted component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,85 +1,66 @@
-import { useAppSelector } from 'hooks';
 import { NavLink } from 'react-router-dom';
-import { selectUser } from 'redux/user';
 import { HiUsers, HiOutlineTruck } from 'react-icons/hi';
 import { MdProductionQuantityLimits, MdWork, MdFeedback } from 'react-icons/md';
 import { GrUserWorker } from 'react-icons/gr';
 import { IconType } from 'react-icons';
-
-enum ROLES {
-  admin = 'admin',
-  applyManager = 'applyManager',
-  servicesManager = 'servicesManager',
-  productsManager = 'productsManager',
-  freeAccess = 'free',
-}
+import { Roles, USER_ROLES } from 'helpers/constants';
+import RestrictedComponent from 'components/RestrictedComponent';
 
 interface ILink {
   href: string;
   title: string;
-  access: ROLES;
+  access: Roles | Roles[];
   icon: IconType;
 }
 
 const navigationLinks: ILink[] = [
-  { href: '/users', title: 'Користувачі', access: ROLES.admin, icon: HiUsers },
+  { href: '/users', title: 'Користувачі', access: Roles.admin, icon: HiUsers },
   {
     href: '/products',
     title: 'Продукти компанії',
-    access: ROLES.freeAccess,
+    access: USER_ROLES,
     icon: MdProductionQuantityLimits,
   },
   {
     href: '/services',
     title: 'Послуги компанії',
-    access: ROLES.freeAccess,
+    access: USER_ROLES,
     icon: HiOutlineTruck,
   },
   {
     href: '/vacancies',
     title: 'Вакансії',
-    access: ROLES.freeAccess,
+    access: USER_ROLES,
     icon: MdWork,
   },
   {
     href: '/resumes',
     title: 'Резюме',
-    access: ROLES.applyManager,
+    access: Roles.applyManager,
     icon: GrUserWorker,
   },
   {
     href: '/feedbacks',
     title: 'Фідбеки',
-    access: ROLES.admin,
+    access: Roles.admin,
     icon: MdFeedback,
   },
 ];
 
 export default function Navigation() {
-  const currentUser = useAppSelector(selectUser);
-  const currentRole = currentUser.user.role;
-
-  const isAccessRights = () => {
-    if (currentRole === ROLES.admin) {
-      return navigationLinks;
-    } else {
-      return navigationLinks.filter(
-        el => el.access === currentRole || el.access === ROLES.freeAccess
-      );
-    }
-  };
-
   return (
     <ul>
-      {isAccessRights().map(({ href, title, icon }, index) => {
+      {navigationLinks.map(({ href, title, access, icon }, index) => {
         const Icon = icon;
         return (
-          <li key={index}>
-            <NavLink to={href}>
-              <Icon />
-              {title}
-            </NavLink>
-          </li>
+          <RestrictedComponent key={index} accessRight={access}>
+            <li>
+              <NavLink to={href}>
+                <Icon />
+                {title}
+              </NavLink>
+            </li>
+          </RestrictedComponent>
         );
       })}
     </ul>
