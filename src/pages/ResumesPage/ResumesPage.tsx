fb2bdc51f4a2ca6829809e4820412b7692d3e32b
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-05-25 19:47:30 +0300

    Create selector for resume filtration

--- src/pages/ResumesPage/ResumesPage.tsx
+++ src/pages/ResumesPage/ResumesPage.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useSearchParams } from 'react-router-dom';
 import { Notify } from 'notiflix';
 import { useAppDispatch, useAppSelector, useModal } from 'hooks';
 import { getAllResumes, selectResumeError } from 'redux/resumes';
+import { getAllVacancyTitles, selectVacancyTitles } from 'redux/vacancies';
 import ResumesGallery from 'components/ResumesGallery';
 import Modal from 'components/Modal';
 import ResumeForm from 'components/ResumeForm';
@@ -11,18 +12,45 @@ export default function ResumesPage() {
   const dispatch = useAppDispatch();
   const { isModalOpen, openModal, closeModal } = useModal();
   const error = useAppSelector(selectResumeError);
+  const titles = useAppSelector(selectVacancyTitles);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const position = searchParams.get('position');
 
   useEffect(() => {
-    dispatch(getAllResumes());
+    dispatch(getAllVacancyTitles());
   }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getAllResumes({ position }));
+  }, [dispatch, position]);
 
   if (error) {
     Notify.failure(error);
   }
 
+  console.log("searchParams.get('position'):\n", searchParams.get('position'));
+
+  const updateQueryString = (position: string) => {
+    const nextParams: {} | { position: string } =
+      position !== '' ? { position } : {};
+    setSearchParams(nextParams);
+  };
+
   return (
     <>
       <h1>This is ResumesPage!</h1>
+      <select
+        onChange={({ target }) => updateQueryString(target.value)}
+        value={position ? position : ''}
+      >
+        <option value="">Chosse option</option>
+        {titles.map(({ _id, title }) => (
+          <option key={_id} value={title}>
+            {title}
+          </option>
+        ))}
+        <option value="other">Other</option>
+      </select>
       <button type="button" onClick={openModal}>
         Create resume
       </button>
--- src/redux/vacancies/selectors.ts
+++ src/redux/vacancies/selectors.ts
@@ -1,2 +1,4 @@
 import type { RootState } from 'redux/store';
 export const selectVacancies = (state: RootState) => state.vacancies;
+
+export const selectVacancyTitles = (state: RootState) => state.vacancies.titles;
