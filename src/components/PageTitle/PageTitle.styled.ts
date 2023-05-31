ad11b1bfdeadc2a50f8a5aa31e0cb3c9fda840f4
Iaroslav Seniuk <savuk7@icloud.com>
2023-05-31 14:50:14 +0300

    added Box component and updated PageTitle comp.

--- src/components/PageTitle/PageTitle.styled.ts
+++ src/components/PageTitle/PageTitle.styled.ts
@@ -5,11 +5,11 @@ export const TitleWrapper = styled.div`
   align-items: center;
   height: 111px; //TODO: тимчасове число, щоб займало менше місця
   padding-left: 34px;
-  background: ${props => props.theme.backgrounds.gradient};
-  border-radius: ${props => props.theme.borders.pageTitle};
+  background: ${props => props.theme.colors.gradientBackground};
+  border-radius: ${props => props.theme.radii.pageTitle};
 `;
 
 export const Title = styled.h1`
-  color: ${props => props.theme.fontColors.pageTitle};
+  color: ${props => props.theme.colors.pageTitleText};
   font-size: ${props => props.theme.fontSizes.xxl};
 `;
