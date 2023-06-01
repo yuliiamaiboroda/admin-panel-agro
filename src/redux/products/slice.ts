baae2d177d6a30fb8b544f698262d7412388d65c
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-06-01 14:19:07 +0300

    Refactored product redux reducers to separate file

--- src/redux/products/slice.ts
+++ src/redux/products/slice.ts
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
 import type { IProduct } from 'helpers/types';
 import {
   getAllProducts,
@@ -8,8 +7,18 @@ import {
   editProduct,
   removeProduct,
 } from './operations';
+import {
+  removeCertainProductReducer,
+  pendingProductReducer,
+  rejectedProductReducer,
+  getAllProductsFulfilledReducer,
+  getCertainProductFulfilledReducer,
+  createProductFulfilledReducer,
+  editProductFulfilledReducer,
+  removeProductFulfilledReducer,
+} from './reducers';
 
-interface IState {
+export interface IState {
   entities: IProduct[];
   certain: IProduct | null;
   isLoading: boolean;
@@ -23,84 +32,29 @@ const initialState: IState = {
   error: null,
 };
 
-const pendingReducer = (state: IState) => ({
-  ...state,
-  isLoading: true,
-  error: null,
-});
-
-const rejectedReducer = (
-  state: IState,
-  action: PayloadAction<string | undefined>
-) => ({
-  ...state,
-  isLoading: false,
-  ...(action.payload ? { error: action.payload } : null),
-});
-
 const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    removeCertainProduct(state) {
-      return { ...state, certain: null };
-    },
+    removeCertainProduct: removeCertainProductReducer,
   },
   extraReducers: builder =>
     builder
-      .addCase(getAllProducts.pending, pendingReducer)
-      .addCase(getAllProducts.fulfilled, (state, action) => {
-        return {
-          ...state,
-          isLoading: false,
-          entities: action.payload,
-        };
-      })
-      .addCase(getAllProducts.rejected, rejectedReducer)
-      .addCase(getCertainProduct.pending, pendingReducer)
-      .addCase(getCertainProduct.fulfilled, (state, action) => {
-        return {
-          ...state,
-          isLoading: false,
-          certain: action.payload,
-        };
-      })
-      .addCase(getCertainProduct.rejected, rejectedReducer)
-      .addCase(createProduct.pending, pendingReducer)
-      .addCase(createProduct.fulfilled, (state, action) => {
-        return {
-          ...state,
-          isLoading: false,
-          entities: [...state.entities, action.payload],
-        };
-      })
-      .addCase(createProduct.rejected, rejectedReducer)
-      .addCase(editProduct.pending, pendingReducer)
-      .addCase(editProduct.fulfilled, (state, action) => {
-        return {
-          ...state,
-          isLoading: false,
-          entities: state.entities.map(product => {
-            if (product._id !== action.payload._id) {
-              return product;
-            }
-            return { ...product, ...action.payload };
-          }),
-          ...(state.certain ? { certain: action.payload } : null),
-        };
-      })
-      .addCase(editProduct.rejected, rejectedReducer)
-      .addCase(removeProduct.pending, pendingReducer)
-      .addCase(removeProduct.fulfilled, (state, action) => {
-        return {
-          ...state,
-          isLoading: false,
-          entities: state.entities.filter(
-            product => product._id !== action.payload
-          ),
-        };
-      })
-      .addCase(removeProduct.rejected, rejectedReducer),
+      .addCase(getAllProducts.pending, pendingProductReducer)
+      .addCase(getAllProducts.fulfilled, getAllProductsFulfilledReducer)
+      .addCase(getAllProducts.rejected, rejectedProductReducer)
+      .addCase(getCertainProduct.pending, pendingProductReducer)
+      .addCase(getCertainProduct.fulfilled, getCertainProductFulfilledReducer)
+      .addCase(getCertainProduct.rejected, rejectedProductReducer)
+      .addCase(createProduct.pending, pendingProductReducer)
+      .addCase(createProduct.fulfilled, createProductFulfilledReducer)
+      .addCase(createProduct.rejected, rejectedProductReducer)
+      .addCase(editProduct.pending, pendingProductReducer)
+      .addCase(editProduct.fulfilled, editProductFulfilledReducer)
+      .addCase(editProduct.rejected, rejectedProductReducer)
+      .addCase(removeProduct.pending, pendingProductReducer)
+      .addCase(removeProduct.fulfilled, removeProductFulfilledReducer)
+      .addCase(removeProduct.rejected, rejectedProductReducer),
 });
 
 export const productsReducer = productsSlice.reducer;
