867fd3bb5e1b506ef039285afd878e6c52e2e53e
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-06-01 17:05:20 +0300

    Create edit/remove button and styled it

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -11,18 +11,44 @@ import {
   position,
   shadow,
 } from 'styled-system';
+import type {
+  TypographyProps,
+  SpaceProps,
+  ColorProps,
+  LayoutProps,
+  FlexboxProps,
+  GridProps,
+  BackgroundProps,
+  BorderProps,
+  PositionProps,
+  ShadowProps,
+} from 'styled-system';
 
-const Box = styled('div')(
-  typography,
-  space,
-  color,
-  layout,
-  flexbox,
-  grid,
-  background,
-  border,
-  position,
-  shadow
-);
+interface IProps
+  extends TypographyProps,
+    SpaceProps,
+    ColorProps,
+    LayoutProps,
+    FlexboxProps,
+    GridProps,
+    BackgroundProps,
+    BorderProps,
+    PositionProps,
+    ShadowProps {
+  children: React.ReactNode;
+}
+
+const Box = styled.div<IProps>`
+  ${typography}
+  ${space}
+  ${color}
+  ${layout}
+  ${flexbox}
+  ${grid}
+  ${background}
+  ${border}
+  ${position}
+  ${shadow}
+`;
 
 export default Box;
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,7 +1,8 @@
-import { Link } from 'react-router-dom';
 import { Roles } from 'helpers/constants';
 import RestrictedComponent from 'components/RestrictedComponent';
 import CardMarkup from 'components/CardMarkup';
+import CardButton from 'components/CardButton';
+import Box from 'components/Box';
 
 interface IProps {
   _id: string;
@@ -24,13 +25,10 @@ export default function ProductCard({
       description={description}
     >
       <RestrictedComponent accessRight={Roles.productsManager}>
-        <Link onClick={event => event.stopPropagation()} to={`${_id}/form`}>
-          Edit
-        </Link>
-        <br />
-        <Link onClick={event => event.stopPropagation()} to={`${_id}/confirm`}>
-          Remove
-        </Link>
+        <Box display="flex" justifyContent="center" gridGap={2}>
+          <CardButton type="edit" navigateTo={`${_id}/form`} />
+          <CardButton type="remove" navigateTo={`${_id}/confirm`} />
+        </Box>
       </RestrictedComponent>
     </CardMarkup>
   );
