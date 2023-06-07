2a36c2d308756974237c7a07f21726636d38f8c3
Iaroslav Seniuk <savuk7@icloud.com>
2023-06-07 16:13:08 +0300

    added hover&focus effects to card

--- src/components/CardWrapperMarkup/CardWrapperMarkup.styled.ts
+++ src/components/CardWrapperMarkup/CardWrapperMarkup.styled.ts
@@ -7,9 +7,17 @@ export const CardWrapper = styled.li`
   border-radius: ${props => props.theme.radii.card};
   box-shadow: ${props => props.theme.shadows.card};
   background-color: ${props => props.theme.colors.primaryBackground};
+  transition: box-shadow 250ms linear;
 
   @media ${devices.tablet} { 
     width: 366px;
     padding: ${props => props.theme.space[6]}px ${props => props.theme.space[2]}px;
+  }
+  
+  :hover,
+  :focus {
+    cursor: pointer;
+    transition: box-shadow 250ms linear;
+    box-shadow: ${props => props.theme.shadows.hoverCard};
   }
 `;
