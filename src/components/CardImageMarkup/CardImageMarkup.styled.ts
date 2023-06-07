f9e516004984ac325f1c995af2d10f3dad4130ce
Iaroslav Seniuk <savuk7@icloud.com>
2023-06-07 15:06:53 +0300

    finish with styling components

--- src/components/CardImageMarkup/CardImageMarkup.styled.ts
+++ src/components/CardImageMarkup/CardImageMarkup.styled.ts
@@ -1,8 +1,13 @@
+import { devices } from 'helpers/constants';
 import styled from 'styled-components';
 
 export const Image = styled.img`
-  width: 348px;
+  width: 330px;
   height: 222px;
   margin-bottom: ${props => props.theme.space[2]}px;
   object-fit: cover; //TODO  cover or contain?
+  
+  @media ${devices.tablet} { 
+    width: 366px;
+  }
 `;
--- src/components/GalleryWrapper/GalleryWrapper.styled.ts
+++ src/components/GalleryWrapper/GalleryWrapper.styled.ts
@@ -1,15 +1,28 @@
+import { devices } from 'helpers/constants';
 import styled from 'styled-components';
-
-export const GalleryWrap = styled.ul`
-  width: 772px;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: ${props => props.theme.space[10]}px;
-  margin-top: -${props => props.theme.space[4]}px;
-`;
 
 export const GalleryCentred = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+export const GalleryWrap = styled.ul`
+  width: 346px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: ${props => props.theme.space[3]}px;
+  margin-top: ${props => props.theme.space[4]}px;
+  
+  @media ${devices.tablet} { 
+    width: 366px;
+    gap: ${props => props.theme.space[6]}px;
+    margin-top: -${props => props.theme.space[4]}px;
+  }
+  
+  @media ${devices.desktop} { 
+    width: 772px;
+    gap: ${props => props.theme.space[10]}px;
+    margin-top: -${props => props.theme.space[7]}px;
+  }
 `;
