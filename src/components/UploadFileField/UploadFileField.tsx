ab98ed0a74bf98b2de661610f10487cd5f5dabde
Iaroslav Seniuk <savuk7@icloud.com>
2023-07-04 02:36:50 +0300

    component styled

--- src/components/UploadFileField/UploadFileField.tsx
+++ src/components/UploadFileField/UploadFileField.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useField, ErrorMessage } from 'formik';
+import { Label, Error } from 'components/FormField/FormField.styled';
+import { Input } from './UploadFileField.styled';
 
 export default function UploadFileField({
   fileRef,
-  label = 'Upload file: ',
+  label = 'Зображення',
   name,
 }: {
   fileRef: React.RefObject<HTMLInputElement>;
   label?: string;
   name: string;
 }) {
-  const [field] = useField(name);
+  const [field, meta] = useField(name);
+  const [selectedFile, setSelectedFile] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFile(e.currentTarget.files?.[0]?.name ?? '');
+  };
 
   return (
-    <>
-      <label>
-        {label}
-        <input type="file" ref={fileRef} {...field} />
-      </label>
-      {/* <br /> */}
-      <ErrorMessage name={field.name} />
-    </>
+    <Label>
+      {label}
+      <input
+        type="file"
+        ref={fileRef}
+        {...field}
+        onChangeCapture={handleChange}
+        hidden
+      />
+      {selectedFile ? (
+        <Input
+          $isInvalid={meta.touched}
+          $isInvalidMsg={meta.error}
+          $isFileSelected={true}
+        >
+          {selectedFile}
+        </Input>
+      ) : (
+        <Input
+          $isInvalid={meta.touched}
+          $isInvalidMsg={meta.error}
+          $isFileSelected={false}
+        >
+          Оберіть зображення
+        </Input>
+      )}
+      <ErrorMessage name={field.name}>
+        {msg => <Error>{msg}</Error>}
+      </ErrorMessage>
+    </Label>
   );
 }
