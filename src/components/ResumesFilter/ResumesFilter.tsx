da52541b89450575ac797e28e4f579368e1541f9
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-07-10 18:50:56 +0300

    Remove additinal requests from resume filter

--- src/components/ResumesFilter/ResumesFilter.tsx
+++ src/components/ResumesFilter/ResumesFilter.tsx
@@ -30,26 +30,44 @@ export default function ResumesFilter({ filterStatus, onSelect }: IProps) {
       />
       <VacancySelector
         onChange={option => {
-          onSelect(({ position, ...rest }) => ({
-            ...rest,
-            ...(option?.value ? { position: option.value } : null),
-          }));
+          onSelect(prevState => {
+            if (Boolean(prevState?.position) === Boolean(option?.value)) {
+              return prevState;
+            }
+            const { position, ...rest } = prevState;
+            return {
+              ...rest,
+              ...(option?.value ? { position: option.value } : null),
+            };
+          });
         }}
       />
       <SortSelector
         onChange={option => {
-          onSelect(({ sort, ...rest }) => ({
-            ...rest,
-            ...(option?.value ? { sort: option.value } : null),
-          }));
+          onSelect(prevState => {
+            if (Boolean(prevState?.sort) === Boolean(option?.value)) {
+              return prevState;
+            }
+            const { sort, ...rest } = prevState;
+            return {
+              ...rest,
+              ...(option?.value ? { sort: option.value } : null),
+            };
+          });
         }}
       />
       <LimitSelector
         onChange={option => {
-          onSelect(({ limit, ...rest }) => ({
-            ...rest,
-            ...(option?.value ? { limit: option.value } : null),
-          }));
+          onSelect(prevState => {
+            if (Boolean(prevState?.limit) === Boolean(option?.value)) {
+              return prevState;
+            }
+            const { limit, ...rest } = prevState;
+            return {
+              ...rest,
+              ...(option?.value ? { limit: option.value } : null),
+            };
+          });
         }}
       />
     </Box>
