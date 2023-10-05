4e91dabfef591ce58f23f1eeaf7e955ee7a6c010
yuliiamaiboroda <111747754+yuliiamaiboroda@users.noreply.github.com>
2023-10-05 15:30:12 +0000

    Deploying to gh-pages from @ yuliiamaiboroda/admin-panel-agro@89923ccdacfbc52f0bd990110c9d7e3e04a169c2 🚀

---
+++ static/js/390.afcc0ac5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadmin_panel_agro=self.webpackChunkadmin_panel_agro||[]).push([[390],{1390:function(a,e,n){n(2791);e.Z=n.p+"static/media/company-logo.d26231fa20bc086e5a542723b5206ddc.svg"}}]);
+//# sourceMappingURL=390.afcc0ac5.chunk.js.map
